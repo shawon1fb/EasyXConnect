0795b27701d41cbbfa9cd7b973d9db2d378597c1
shawon1fb <shawon0187@gmail.com>
2024-02-24 18:22:11 +0600

    multi part dto

--- Sources/EasyXConnect/Core/Abstractions/DtoProtocall.swift
+++ Sources/EasyXConnect/Core/Abstractions/DtoProtocall.swift
@@ -6,30 +6,92 @@
 //
 
 import Foundation
-
-protocol DTO: Encodable{
-    func toData()-> Data?
+// Define the AnyEncodable type
+public struct AnyEncodable: Encodable {
+    
+    public let value: Encodable
+    
+    public init(_ value: Encodable) {
+        self.value = value
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        try value.encode(to: encoder)
+    }
 }
 
-protocol MultipartDTO:DTO{
-//    let boundary:String = ""  //= FormDataHelper.generateBoundary()
+public protocol DTO: Encodable{
+    func toData()-> Data?
+    func toJsonMap() -> [String: AnyEncodable]?
+    func toQueryParams() -> [String: String]?
+}
+
+public protocol MultipartDTO: DTO{
+    //    var boundary: String { get }
+    // let boundary:String = FormData.generateBoundary()
+    
+    var boundary:String { get set }
+}
+
+
+
+extension MultipartDTO{
+    
+    
+    public  func getHeader()->[String:String]{
+        //let boundary:String = generateBoundary()
+        print("boundary header -> ",boundary)
+        return [
+            "Content-Type" : "multipart/form-data; boundary=\(boundary)",
+            "Accept" : "application/json"
+        ]
+    }
+    
+    public func toData()->Data?{
+        if let map = toJsonMap(){
+            //  let boundary:String = generateBoundary()
+            print("boundary -> ",boundary)
+            let formData = FormData(map: map, boundary: boundary )
+            return formData.toBytes()
+        }
+        return nil
+    }
 }
 
 extension DTO{
     
-    func toData()-> Data?{
-        do{
-            let encoder = JSONEncoder()
-            let data = try encoder.encode(self)
-            return data
-        }catch{
-            debugPrint("encodable error is => \(error) ")
+    public func toJsonMap() -> [String: AnyEncodable]? {
+        let mirror = Mirror(reflecting: self)
+        var map = [String: AnyEncodable]()
+        
+        for case let (label?, value) in mirror.children {
+            // Handle optional and non-encodable values gracefully:
+            if let encodableValue = value as? Encodable {
+                map[label] = AnyEncodable(encodableValue)
+            } else {
+                // Optionally handle non-encodable values here if needed
+                // For example, you could convert them to string representations:
+                map[label] = AnyEncodable(String(describing: value))
+            }
         }
         
-        return nil
+        return map
     }
     
-    func toString()-> String{
+    public func toData() -> Data? {
+        guard let map = toJsonMap() else { return nil }
+        
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(map)
+            return data
+        } catch {
+            debugPrint("Encoding error: \(error)")
+            return nil
+        }
+    }
+    
+    public func toString()-> String{
         
         if let data = toData(){
             if let jsonString = String(data: data, encoding: .utf8) {
@@ -40,36 +102,31 @@ extension DTO{
         return "\(self)"
     }
     
-    func toQueryParams() -> [String: String]? {
-        if let data = toData(),
-           let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+    public func toQueryParams() -> [String: String]? {
+        if
+            let jsonObject = toJsonMap() {
             
             var queryParams: [String: String] = [:]
-            
             for (key, value) in jsonObject {
-                if let stringValue = value as? String {
+                if let stringValue = value.value as? String {
                     queryParams[key] = stringValue
-                } else if let numberValue = value as? NSNumber {
+                } else if let numberValue = value.value as? NSNumber {
                     queryParams[key] = "\(numberValue)"
-                } else if let boolValue = value as? Bool {
+                } else if let boolValue = value.value as? Bool {
                     queryParams[key] = boolValue ? "true" : "false"
                 }
-                // You can add additional type conversions as needed
+                else if let value = value.value as? DTO {
+                    queryParams[key] = value.toString()
+                }
+                else {
+                    queryParams[key] = "\(value.value)"
+                }
             }
-            
             return queryParams.isEmpty ? nil : queryParams
         }
         
         return nil
     }
-
-
-    
-    
-}
-
-
-struct FromData{
     
 }
 
---
+++ Sources/EasyXConnect/Core/multipart/MultipartFile.swift
@@ -0,0 +1,113 @@
+//
+//  MultipartFile.swift
+//
+//
+//  Created by shahanul on 24/2/24.
+//
+import Foundation
+
+public struct MultipartFile {
+    let data: Data
+    let filename: String
+    let contentType: String
+}
+
+extension URL{
+    public func toMultipartFile() -> MultipartFile? {
+        do {
+            // Read file data from URL
+            let data = try Data(contentsOf: self)
+            
+            // Extract filename from URL
+            let filename = lastPathComponent
+            
+            // Determine content type based on file extension
+            var contentType = "application/octet-stream"
+            if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as CFString, nil)?.takeRetainedValue(),
+               let mimetype = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType)?.takeRetainedValue() {
+                contentType = mimetype as String
+            }
+            
+            return MultipartFile(data: data, filename: filename, contentType: contentType)
+        } catch {
+            print("Error creating MultipartFile: \(error)")
+            return nil
+        }
+    }
+}
+
+public struct FormData {
+    private static let maxBoundaryLength = 70
+    private static let boundaryCharacters = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    
+    public static func generateBoundary() -> String {
+        let randomBoundary = String((0..<maxBoundaryLength).map { _ in boundaryCharacters.randomElement()! })
+        return "----WebKitFormBoundary\(randomBoundary)"
+    }
+    
+    let boundary: String
+    var fields = [(String, String)]()
+    var files = [(String, MultipartFile)]()
+    
+    public init(map: [String: AnyEncodable?] , boundary:String ) {
+        self.boundary = boundary //FormData.generateBoundary()
+        
+        map.forEach { key, value in
+            guard let value = value?.value else { return }
+            
+            if let file = value as? URL {
+                if let multipartFile = file.toMultipartFile(){
+                    files.append((key, multipartFile))
+                }
+                
+            } else if let filesArray = value as? [URL] {
+                for url in filesArray{
+                    if let file = url.toMultipartFile(){
+                        files.append((key, file))
+                    }
+                }
+                
+            } else if let valuesArray = value as? [Any] {
+                valuesArray.forEach { fields.append((key, String(describing: $0))) }
+            } else {
+                fields.append((key, String(describing: value)))
+            }
+        }
+    }
+    
+    
+    public func toBytes() -> Data? {
+        var data = Data()
+        
+        // Start building the multipart form data
+        let boundaryPrefix = "--\(boundary)\r\n"
+        
+        // Add fields data
+        for field in fields {
+            data.append(boundaryPrefix.data(using: .utf8)!)
+            data.append("Content-Disposition: form-data; name=\"\(field.0)\"\r\n\r\n".data(using: .utf8)!)
+            data.append("\(field.1)\r\n".data(using: .utf8)!)
+        }
+        
+        // Add files data
+        for file in files {
+            data.append(boundaryPrefix.data(using: .utf8)!)
+            data.append("Content-Disposition: form-data; name=\"\(file.0)\"; filename=\"\(file.1.filename)\"\r\n".data(using: .utf8)!)
+            data.append("Content-Type: \(file.1.contentType)\r\n\r\n".data(using: .utf8)!)
+            data.append(file.1.data)
+            data.append("\r\n".data(using: .utf8)!)
+        }
+        
+        // Add the final boundary indicating the end of the multipart form data
+        data.append("--\(boundary)--\r\n".data(using: .utf8)!)
+        
+        return data
+    }
+
+}
+
+// Example usage:
+//let formData = FormData(map: ["key1": "value1", "key2": 123, "file1": MultipartFile(data: Data("file1content".utf8), filename: "example.txt", contentType: "text/plain")])
+//
+//let data = formData.toBytes()
+//print(data.count)
