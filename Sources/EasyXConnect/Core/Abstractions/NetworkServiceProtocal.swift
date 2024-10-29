f6cab97f09ede94bbad08ec3d592c78363b9c1f6
Shahanul <shahanul.ssl@gmail.com>
2024-10-29 09:44:08 +0600

    NetworkServiceProtocal make public

--- Sources/EasyXConnect/Core/Abstractions/NetworkServiceProtocal.swift
+++ Sources/EasyXConnect/Core/Abstractions/NetworkServiceProtocal.swift
@@ -5,27 +5,24 @@
 //  Created by shahanul on 24/2/24.
 //
 import Foundation
-
-protocol NetworkServiceProtocal{
-    func getException(status:Int, message:String?)throws
+public protocol NetworkServiceProtocal {
+    func getException(status: Int, message: String?) throws
 }
 
-extension NetworkServiceProtocal{
+extension NetworkServiceProtocal {
     
-    func getException(status:Int, message:String?)throws{
+    public func getException(status: Int, message: String?) throws {
         switch status {
             case 400:
-                throw DefaultException(message: message == nil ? "Bad Request" : message!)
+                throw DefaultException(message: message ?? "The server could not process the request due to invalid input.")
             case 401:
-                throw UnAuthorizedException(message: message == nil ? "Unauthorized request" : message!)
+                throw UnAuthorizedException(message: message ?? "Access denied. Please check your credentials and try again.")
             case 403:
-                throw ForbiddenException(message: message == nil ? "Forbidden request" : message!)
+                throw ForbiddenException(message: message ?? "You do not have the necessary permissions to access this resource.")
             case 500:
-                throw ServerException(message: message == nil ? "server error" : message!)
+                throw ServerException(message: message ?? "An unexpected error occurred on the server. Please try again later.")
             default:
-                throw DefaultException(message: message == nil ? "something went wrong" : message!)
+                throw DefaultException(message: message ?? "An unexpected error occurred. Please contact support if the problem persists.")
         }
-        
     }
 }
-
